0d2c83aa3ff9a070e8e026b561985381f57c9cb3
Max Lotz <maxlotz@gmail.com>
2016-09-30 00:32:03 +0200

    added scope names and loss as a summary so it can be viewed in tensorboard

--- 2cnn.py
+++ 2cnn.py
@@ -1,9 +1,3 @@
-'''
-Testing status
-read_labeled_image_list definitely works
-input_pipeline definitely works
-'''
-
 import tensorflow as tf
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import dtypes
@@ -61,36 +55,45 @@ def input_pipeline(image_list, label_list):
 	Notes:
 		I decided to leave the output labels as ints, not as onehot as many tensorflow functions use ints instead of onehot, so onehot can be used when needed.
     '''
+	with tf.name_scope('input'):
+		images = ops.convert_to_tensor(image_list, dtype=dtypes.string)
+		labels = ops.convert_to_tensor(label_list, dtype=dtypes.string)
 
-	images = ops.convert_to_tensor(image_list, dtype=dtypes.string)
-	labels = ops.convert_to_tensor(label_list, dtype=dtypes.string)
+		image_queue, label_queue = tf.train.slice_input_producer(
+			                                [images, labels],
+			                                shuffle=True)
+		#File reader and decoder for images and labels goes here
+		with tf.name_scope('image'): 
 
-	input_queue = tf.train.slice_input_producer(
-		                                [images, labels],
-		                                shuffle=True)
-	#File reader and decoder for images and labels goes here
-	image_filename = input_queue[0]
-	file_content = tf.read_file(image_filename)
-	image_data = tf.image.decode_jpeg(file_content, channels=3)
-	label_data = input_queue[1]
+			with tf.name_scope('decode'): 
+				file_content = tf.read_file(image_queue)
+				image_data = tf.image.decode_jpeg(file_content, channels=3)
+		
+			#Any resizing or processing (eg. normalising) goes here
+			with tf.name_scope('normalize'): 
+				image_data = tf.cast(image_data, tf.float32)
+				mean, variance = tf.nn.moments(image_data, [0, 1, 2])
+				image_data = (image_data - mean) #/ tf.sqrt(variance)
+				image_data.set_shape([IMAGE_HEIGHT, IMAGE_WIDTH, CHANNELS])
 
-	#Any resizing or processing (eg. normalising) goes here 
-	image_data = tf.cast(image_data, tf.float32)
-	mean, variance = tf.nn.moments(image_data, [0, 1, 2])
-	image_data = (image_data - mean) #/ tf.sqrt(variance)
-	label_data = tf.string_to_number(label_data, out_type=tf.int32)
-	
-	image_data.set_shape([IMAGE_HEIGHT, IMAGE_WIDTH, CHANNELS])
+		
+		with tf.name_scope('label'): 
 
-	image_batch, label_batch, image_filenames = tf.train.shuffle_batch(
-		                                [image_data, label_data, image_filename],
-		                                batch_size=BATCH_SIZE,
-										capacity=CAPACITY,
-                                        min_after_dequeue=MIN_AFTER_DEQUEUE,
-		                                #,num_threads=1
-		                                )
+			with tf.name_scope('decode'): 
+				label_data = tf.string_to_number(label_queue, out_type=tf.int32)
 
-	return image_batch, label_batch, image_filenames
+		image_batch, label_batch = tf.train.shuffle_batch(
+			                                [image_data, label_data],
+			                                batch_size=BATCH_SIZE,
+											capacity=CAPACITY,
+	                                        min_after_dequeue=MIN_AFTER_DEQUEUE,
+			                                #,num_threads=1
+			                                )
+
+		tf.image_summary('image', image_batch, 3)
+        tf.histogram_summary('image', image_batch)
+
+	return image_batch, label_batch
 
 # use this as the input to weights_variable to get a xavier normal distribution
 # see Glorot, Bengio 2010 http://jmlr.org/proceedings/papers/v9/glorot10a/glorot10a.pdf
@@ -104,12 +107,12 @@ def weights_xav_uniform_variable(shape, fan_in, fan_out):
 	return tf.Variable(initial)
 
 def weights_variable(shape, std):
-	initial = tf.truncated_normal(shape, stddev=std)
+	initial = tf.truncated_normal(shape, stddev=std, name='weights')
 	return tf.Variable(initial)
 
 # bias initialised as 0
 def bias_variable(shape):
-  initial = tf.constant(0.0, shape=shape)
+  initial = tf.constant(0.0, shape=shape, name='biases')
   return tf.Variable(initial)
 
 def conv2d(x, W):
@@ -117,7 +120,7 @@ def conv2d(x, W):
 
 def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+                        strides=[1, 2, 2, 1], padding='SAME', name="pool")
 
 # returns 1 for each batch image that was in top k predictions, 0 for each one that isn't. output type = tf.float32
 def topk(prediction, target, k):
@@ -131,12 +134,11 @@ def topk(prediction, target, k):
 #validation_image_batch, validation_label_batch = input_pipeline(validation_images, validation_labels)
 
 train_images, train_labels = read_labeled_image_list(training_file)
-train_image_batch, train_label_batch, train_image_filenames = input_pipeline(train_images, train_labels)
+train_image_batch, train_label_batch = input_pipeline(train_images, train_labels)
 train_image_batch = train_image_batch
 
 print "input pipeline ready"
 
-f = train_image_filenames
 x = train_image_batch
 y = train_label_batch # use this for accuracy
 y_onehot = tf.one_hot(y,NUM_CLASSES) # use in one-hot format for loss
@@ -146,79 +148,99 @@ y_onehot = tf.one_hot(y,NUM_CLASSES) # use in one-hot format for loss
 
 #conv layer 1
 #w_conv1 = weights_variable([3,3,3,128],xavier(3*3*128))
-w_conv1 = weights_variable([3,3,3,128], 0.02)
-b_conv1 = bias_variable([128])
-h_conv1 = tf.nn.relu(conv2d(x,w_conv1) + b_conv1)
+with tf.name_scope('conv1') as scope:
+	w = weights_variable([3,3,3,128], 0.02)
+	b = bias_variable([128])
+	conv1 = tf.nn.relu(conv2d(x,w) + b, name="activations")
 
 #conv layer 2
 #w_conv2 = weights_xav_uniform_variable([3,3,128,128],3*3*128,3*3*128)
-w_conv2 = weights_variable([3,3,128,128], 0.02)
-b_conv2 = bias_variable([128])
-h_conv2 = tf.nn.relu(conv2d(h_conv1,w_conv2) + b_conv2)
+with tf.name_scope('conv2') as scope:
+	w = weights_variable([3,3,128,128], 0.02)
+	b = bias_variable([128])
+	conv2 = tf.nn.relu(conv2d(conv1,w) + b, name="activations")
 
 #maxpool1, images now 32x32
-h_pool1 = max_pool_2x2(h_conv2)
+with tf.name_scope('pool1') as scope:
+	pool1 = max_pool_2x2(conv2)
 
 #conv layer 3
 #w_conv3 = weights_xav_uniform_variable([3,3,128,128],3*3*128,3*3*128)
-w_conv3 = weights_variable([3,3,128,128], 0.01)
-b_conv3 = bias_variable([128])
-h_conv3 = tf.nn.relu(conv2d(h_pool1,w_conv3) + b_conv3)
+with tf.name_scope('conv3') as scope:
+	w = weights_variable([3,3,128,128], 0.01)
+	b = bias_variable([128])
+	conv3 = tf.nn.relu(conv2d(pool1,w) + b, name="activations")
 
 #conv layer 4
 #w_conv4 = weights_xav_uniform_variable([3,3,128,128],3*3*128,3*3*128)
-w_conv4 = weights_variable([3,3,128,128], 0.01)
-b_conv4 = bias_variable([128])
-h_conv4 = tf.nn.relu(conv2d(h_conv3,w_conv4) + b_conv4)
+with tf.name_scope('conv4') as scope:
+	w = weights_variable([3,3,128,128], 0.01)
+	b = bias_variable([128])
+	conv4 = tf.nn.relu(conv2d(conv3,w) + b, name="activations")
 
 #maxpool2, images now 16x16
-h_pool2 = max_pool_2x2(h_conv4)
+with tf.name_scope('pool2') as scope:
+	pool2 = max_pool_2x2(conv4)
 
 #conv layer 5
 #w_conv5 = weights_xav_uniform_variable([3,3,128,128],3*3*128,400)
-w_conv5 = weights_variable([3,3,128,128], 0.01)
-b_conv5 = bias_variable([128])
-h_conv5 = tf.nn.relu(conv2d(h_pool2,w_conv5) + b_conv5)
+with tf.name_scope('conv5') as scope:
+	w = weights_variable([3,3,128,128], 0.01)
+	b = bias_variable([128])
+	conv5 = tf.nn.relu(conv2d(pool2,w) + b, name="activations")
 
 #maxpool3, imags now 8x8
-h_pool3 = max_pool_2x2(h_conv5)
+with tf.name_scope('pool3') as scope:
+	pool3 = max_pool_2x2(conv5)
 
 #fully connected layer 1
 #w_fc1 = weights_xav_uniform_variable([8*8*128,400],400,400)
-w_fc1 = weights_variable([8*8*128,400], 0.02)
-b_fc1 = bias_variable([400])
-h_pool_3_flat = tf.reshape(h_pool3,[-1,8*8*128])
-t_fc1 = tf.matmul(h_pool_3_flat, w_fc1) + b_fc1
-h_fc1 = tf.nn.relu(t_fc1)
-#h_fc1 = tf.nn.relu(tf.matmul(h_pool_3_flat, w_fc1) + b_fc1)
+with tf.name_scope('fully_connected1') as scope:
+	w = weights_variable([8*8*128,400], 0.02)
+	b = bias_variable([400])
+	pool3_flat = tf.reshape(pool3,[-1,8*8*128])
+	fully_connected1 = tf.nn.relu(tf.matmul(pool3_flat, w) + b, name="activations")
 
 #fully connected layer 2
 #w_fc2 = weights_xav_uniform_variable([400,400],400,200)
-w_fc2 = weights_variable([400,400], 0.02)
-b_fc2 = bias_variable([400])
-h_fc2 = tf.nn.relu(tf.matmul(h_fc1, w_fc2) + b_fc2)
+with tf.name_scope('fully_connected2') as scope:
+	w = weights_variable([400,400], 0.02)
+	b = bias_variable([400])
+	fully_connected2 = tf.nn.relu(tf.matmul(fully_connected1, w) + b, name="activations")
 
 #fully connected layer 3
 #w_fc3 = weights_variable([400,200],xavier(400))
-w_fc3 = weights_variable([400,200], 0.04)
-b_fc3 = bias_variable([200])
-h_fc3 = tf.nn.relu(tf.matmul(h_fc1, w_fc3) + b_fc3)
+with tf.name_scope('fully_connected3') as scope:
+	w = weights_variable([400,200], 0.04)
+	b = bias_variable([200])
+	fully_connected3 = tf.nn.relu(tf.matmul(fully_connected2, w) + b, name="activations")
 
 #prediction layer
-y_pred = tf.nn.softmax(h_fc3)
+with tf.name_scope('softmax') as scope:
+	y_pred = tf.nn.softmax(fully_connected3, name="softmax")
 
 #loss measure: cross entropy
-loss = tf.reduce_mean(-tf.reduce_sum(y_onehot * tf.log(y_pred), reduction_indices=[1]))
+with tf.name_scope('loss') as scope:
+	loss = tf.reduce_mean(-tf.reduce_sum(y_onehot * tf.log(y_pred), reduction_indices=[1]), name="loss")
 
 #optimizer here
 train_step = tf.train.GradientDescentOptimizer(0.8).minimize(loss)
 
 #accuracy measure here
 
+# summary for loss
+tf.scalar_summary("cross entropy loss", loss)
+summary_op = tf.merge_all_summaries()
+
 with tf.Session() as sess:
   
   # initialize the variables
   sess.run(tf.initialize_all_variables())
+
+  # create log writer object, this writes the data to the directory /logs
+  # to visualise enter the following into terminal: tensorboard --logdir=logs/ --port=6006
+  # then navigate to http://0.0.0.0:6006/
+  writer = tf.train.SummaryWriter("logs/", graph=tf.get_default_graph())
   
   # initialize the queue threads to start to shovel data
   coord = tf.train.Coordinator()
@@ -229,15 +251,13 @@ with tf.Session() as sess:
   # print ashape_
   # print "\n"
   # print a
-  test_s, test_, test2_s, test2_ = sess.run([tf.shape(test), test, tf.shape(test2), test2])
-  
-  print test_s
-  print test2_s
 
-  # # RUN CODE HERE
-  # for i in range(NUM_ITERATIONS):
-  # 	_, x = sess.run([train_step, loss])
-  # 	print "iteration: " + str(i*100) + "\t" + "loss: " + str(x)
+  # RUN CODE HERE
+  for i in range(NUM_ITERATIONS):
+  	_, summary = sess.run([train_step, summary_op])
+  	# tensorboard wont show the x axis value if its an int
+  	writer.add_summary(summary, float(i*100))
+  	print "iter " + str((i+1)*100) + " complete"
 
   # stop our queue threads and properly close the session
   coord.request_stop()
